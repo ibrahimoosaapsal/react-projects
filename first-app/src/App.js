a134931b19908c80919e8e9516d700a921cf4a66
ibrahim <you@example.com>
2023-11-11 11:08:10 +0530

    changes happen

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,53 @@
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-
+import { useState } from "react";
 function App() { 
+ const [items,setitems]=useState( //creating two variables one is for haolding default value another one is for manupulated array value 
+     [//we can creating a array for default value
+       {//we can creating three objects for this default array
+         id:1,
+         checked:true,
+         listname:"learn react"
+       },
+       {
+         id:2,
+         checked:false,
+         listname:"learn python"
+       },
+       {
+         id:3,
+         checked:true,
+         listname:"learn about ai"
+       }
+     ]);
+ 
+    const  handleCheckBox =(id)=>{//creating a function for input field
+     const newarray=items.map((item)=> //creating a new array for 2nd useState value, 
+     //and this item stand for like looping i variable it will collect the three variable one by one
+     item.id===id ? {...item,checked:!item.checked} : item) //this is the logic
+     setitems(newarray)// setting the new aaray to 2nd use state variable
+     localStorage.setItem("list_names",JSON.stringify(newarray)) //save this new array to local storage
+    }
+     
+    const handleDelete= (id)=>{ //creating a function for delete icon,and this fun  will remove the matching id.
+     const newarray=items.filter((item)=>
+     item.id!==id)
+     setitems(newarray)
+     localStorage.setItem("list_names",JSON.stringify(newarray))
+   }
+     
+ 
    return (
      <div className="App">
           <Header /> 
-          <Content />
+          <Content
+           item={items}
+           setitems={setitems}
+           handleCheckBox={handleCheckBox}
+           handleDelete={setitems}
+          
+          />
           <Footer />
      
      </div>
--- first-app/src/Content.js
+++ first-app/src/Content.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import {  FaTrash } from "react-icons/fa";
 
-const Content = () => {
-    function handleNameChange(){
-       const names=["grow","make","get","earn"];
-       const int=[Math.floor(Math.random()*4)];   
-       return names[int];  
-    }
+
+const Content = (item,setitems,handleCheckBox,handleDelete) => {//creating a component which is functional component
+  
+  
   return (
     <main>
-      
-    <p>lets {handleNameChange()} money</p>
-
+      {(item.length)?//1A:this logic will check the list length
+      (//if list have any object it will pass 
+      <ul>
+       {item.map((item) =>//map is like the shortand property of loop 
+         <li className='item' key={item.id}>
+          <input
+          type='checkbox'
+          checked={item.checked}
+          onChange={() => handleCheckBox(item.id)}
+          />
+          <label
+          onDoubleClick={() => handleCheckBox(item.id)}
+          style={(item.checked)? 
+            {textDecoration:"line-through"}:null}
+          >{item.listname}</label>
+          <FaTrash
+             role='button'
+             tabIndex="0"
+             onClick={()=>handleDelete(item.id)}
+          />
+         </li>       
+       )}
+     </ul>
+      )://1B if the condition is false 
+      <p>your list is empty </p>//it will print the else statement
+          }
     </main>
   )
 }
